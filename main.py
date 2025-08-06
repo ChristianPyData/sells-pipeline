b787be721f2f4ecff82f31e1ba31030bada597f2
Cristian Garcia <cgcristiangarcia07@gmail.com>
2025-08-06 04:45:27 -0500

    subida inicial

--- main.py
+++ main.py
@@ -4,17 +4,10 @@ from datetime import datetime
 import duckdb  
 
 FECHA = datetime.now().strftime("%Y%m%d")
-PATH = "data/ventas.csv"
+PATH = "./data/ventas.csv"
+
 df = pd.read_csv(PATH)
 errores = []
-con = duckdb.connect("db/ventas.db")
-tabla = f"ventas_limpias_{FECHA}"
-
-con.execute(f"""
-CREATE TABLE IF NOT EXISTS {tabla} AS
-SELECT * FROM read_csv_auto('data/limpio/ventas_limpias_{FECHA}.csv')
-""")
-con.close()
 
 print("✅ Pipeline ejecutado con éxito.")
 # Fecha inválida
@@ -32,6 +25,10 @@ errores.append(df[df.isna().any(axis=1)].assign(error='Nulos'))
 
 # Unir errores
 errores_df = pd.concat(errores).drop_duplicates()
+#crear carpetas si no existen
+os.makedirs("logs", exist_ok=True)
+os.makedirs("data/limpio", exist_ok=True)
+os.makedirs("kpis", exist_ok=True)
 
 # Guardar log de errores
 errores_df.to_csv(f"logs/errores_{FECHA}.csv", index=False)
@@ -40,6 +37,7 @@ errores_df.to_csv(f"logs/errores_{FECHA}.csv", index=False)
 df_limpio = df[~df.index.isin(errores_df.index)]
 df_limpio.to_csv(f"data/limpio/ventas_limpias_{FECHA}.csv", index=False)
 
+# KPIs
 kpis = {
     "fecha_corte": FECHA,
     "total_ventas": df_limpio["monto"].sum(),
@@ -47,5 +45,16 @@ kpis = {
     "ticket_maximo": df_limpio["monto"].max(),
     "region_top": df_limpio.groupby("region")["monto"].sum().idxmax()
 }
-
 pd.DataFrame([kpis]).to_csv(f"kpis/resumen_kpis_{FECHA}.csv", index=False)
+
+# Carga a DuckDB
+con = duckdb.connect("db/ventas.db")
+tabla = f"ventas_limpias_{FECHA}"
+
+con.execute(f"""
+CREATE TABLE IF NOT EXISTS {tabla} AS
+SELECT * FROM read_csv_auto('data/limpio/ventas_limpias_{FECHA}.csv')
+""")
+con.close()
+
+print("✅ Pipeline ejecutado con éxito.")
